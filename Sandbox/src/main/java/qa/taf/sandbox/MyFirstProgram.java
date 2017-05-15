512e5350e946b5230a1538654f202499ac05a301
lamerushman <kirill.puhalskiy@gmail.com>
2017-05-15 16:35:00 +0300

    New functions added

--- Sandbox/src/main/java/qa/taf/sandbox/MyFirstProgram.java
+++ Sandbox/src/main/java/qa/taf/sandbox/MyFirstProgram.java
@@ -4,8 +4,23 @@ public class MyFirstProgram {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		hello("Kirill");
+		double l = 5;
+		System.out.println("SQ = "+ area(l));
+		double a = 4;
+		double b = 6;
+		System.out.println("SQ = " + area(a,b));
 
 	}
+	public static void hello(String somebody){
 
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area (double l){
+		return l * l;
+	}
+	public static double area(double a, double b){
+		return a * b;
+	}
 }
